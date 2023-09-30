d0cc401585f1127951589d5e0ba2dd50712bf3f5
Dengyu Wu <d.wooo92@gmail.com>
2023-09-30 16:49:14 +0800

    update

---
+++ easycutoff/functions.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import random
+import os
+import numpy as np
+
+def TET_loss(outputs, labels, criterion, means, lamb):
+    T = outputs.size()[0]
+    Loss_es = 0
+
+    for t in range(T):
+        Loss_es += criterion(outputs[t, ...], labels)
+    Loss_es = Loss_es / T # L_TET  
+    if lamb != 0:
+        MMDLoss = torch.nn.MSELoss()
+        y = torch.zeros_like(outputs).fill_(means)
+        Loss_mmd = MMDLoss(outputs, y) # L_mse
+    else:
+        Loss_mmd = 0
+    return (1 - lamb) * Loss_es + lamb * Loss_mmd # L_Total
+
+
+class LabelSmoothing(nn.Module):
+    """
+    NLL loss with label smoothing.
+    """
+    def __init__(self, smoothing=0.1):
+        """
+        Constructor for the LabelSmoothing module.
+        :param smoothing: label smoothing factor
+        """
+        super(LabelSmoothing, self).__init__()
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+
+    def forward(self, x, target):
+        logprobs = torch.nn.functional.log_softmax(x, dim=-1)
+        nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1))
+        nll_loss = nll_loss.squeeze(1)
+        smooth_loss = -logprobs.mean(dim=-1)
+        loss = self.confidence * nll_loss + self.smoothing * smooth_loss
+        return loss.mean()
