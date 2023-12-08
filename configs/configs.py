758cbf53d76818a1f0a377f02b1a3c15710363ff
Dengyu Wu <d.wooo92@gmail.com>
2023-12-09 06:06:30 +0800

    update

--- configs/configs.py
+++ configs/configs.py
@@ -17,19 +17,37 @@ class BaseConfig(BaseModel):
     gpu_id: str
     nprocs: int
     local_rank: int
-    
+    data: str
+    model: str
+    dataset_path: str
+
+
 class SNNConfig(BaseModel):
+    method: str
+    ann_constrs: str
     T: int
     evaluate: bool
     TET: bool
+    regularizer: str
     means: float
     lamb: float
     alpha: float
+    multistep: bool
+    
+class SNNTest(BaseModel):
+    T: int
+    sigma: float
+    mode: str
+    model_path: str
 
 class LoggingConfig(BaseModel):
     wandb_logging: bool
     tensorboard_logging: bool
+    comet_logging: bool
     run_dir: str
 
 class AllConfig(BaseConfig, SNNConfig,LoggingConfig):
+    pass
+
+class TestConfig(BaseConfig, SNNTest):
     pass
