415e89e36010a8bf5d6e5c14aef63d545d335cad
Dengyu Wu <d.wooo92@gmail.com>
2023-12-11 04:09:41 +0800

    include frame-based experiment

--- scripts/SNN_Training/baseline/cifar10-dvs/baseline_training.sh
+++ scripts/SNN_Training/baseline/cifar10-dvs/baseline_training.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 python  ./scripts/training.py \
+        base.batch_size=128 \
         base.epochs=300 \
-        base.gpu_id=\'2\' \
+        base.gpu_id=\'1\' \
         base.seed=1200 \
-        base.port=\'14152\' \
+        base.port=\'11152\' \
         base.data=\'cifar10-dvs\' \
-        base.model=\'vggsnn\' \
+        base.model=\'vggann\' \
         base.dataset_path=\'/LOCAL/dengyu/dvs_dataset/dvs-cifar10\'\
         \
         snn-train.method=\'snn\' \
-        snn-train.ann_constrs=\'baseconstrs\' \
+        snn-train.ann_constrs=\'none\' \
+        snn-train.snn_layers=\'baselayer\' \
         snn-train.regularizer=\'none\' \
+        snn-train.TET=True \
         snn-train.multistep=True \
         snn-train.T=10 \
-        snn-train.alpha=0.00 
+        snn-train.alpha=0.00
