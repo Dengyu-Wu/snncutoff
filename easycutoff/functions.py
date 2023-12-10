4b226b2e066d56d64b870cffa01a04f207cd11f9
Dengyu Wu <d.wooo92@gmail.com>
2023-12-11 07:12:13 +0800

    update SNNROE

--- easycutoff/functions.py
+++ easycutoff/functions.py
@@ -1,24 +1,5 @@
 import torch
 import torch.nn as nn
-import random
-import os
-import numpy as np
-
-def TET_loss(outputs, labels, criterion, means, lamb):
-    T = outputs.size()[0]
-    Loss_es = 0
-
-    for t in range(T):
-        Loss_es += criterion(outputs[t, ...], labels)
-    Loss_es = Loss_es / T # L_TET  
-    if lamb != 0:
-        MMDLoss = torch.nn.MSELoss()
-        y = torch.zeros_like(outputs).fill_(means)
-        Loss_mmd = MMDLoss(outputs, y) # L_mse
-    else:
-        Loss_mmd = 0
-    return (1 - lamb) * Loss_es + lamb * Loss_mmd # L_Total
-
 
 class LabelSmoothing(nn.Module):
     """
