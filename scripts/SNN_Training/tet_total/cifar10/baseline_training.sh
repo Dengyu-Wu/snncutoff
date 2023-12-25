58c865c7441c39ed0b1e64eb74669ab34f8e0301
Dengyu Wu <d.wooo92@gmail.com>
2023-12-25 17:31:11 +0800

    update

---
+++ scripts/SNN_Training/tet_total/cifar10/baseline_training.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+python  ./scripts/training.py \
+        base.batch_size=128 \
+        base.epochs=300 \
+        base.gpu_id=\'1\' \
+        base.seed=1200 \
+        base.port=\'11152\' \
+        base.data=\'cifar10\' \
+        base.model=\'vgg11\' \
+        base.dataset_path=\'datasets\' \
+        \
+        snn-train.method=\'snn\' \
+        snn-train.ann_constrs=\'baseconstrs\' \
+        snn-train.snn_layers=\'baselayer\' \
+        snn-train.regularizer=\'roe\' \
+        snn-train.TET=True \
+        snn-train.multistep=True \
+        snn-train.add_time_dim=True \
+        snn-train.T=4 \
+        snn-train.alpha=0.00
