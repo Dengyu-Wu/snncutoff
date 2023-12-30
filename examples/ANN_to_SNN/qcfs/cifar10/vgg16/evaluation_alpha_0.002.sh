552f913f798e4e44036968d81a5c2cff2ea2e930
Dengyu Wu <d.wooo92@gmail.com>
2023-12-30 23:04:54 +0800

    move configs to snncutoff

---
+++ examples/ANN_to_SNN/qcfs/cifar10/vgg16/evaluation_alpha_0.002.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+python  ./snncutoff/scripts/evaluation.py \
+        base.epochs=300 \
+        base.gpu_id=\'1\' \
+        base.seed=1200 \
+        base.port=\'13152\' \
+        base.data=\'cifar10\' \
+        base.model=\'vgg16\' \
+        base.dataset_path='datasets' \
+        \
+        snn-train.method=\'ann\' \
+        snn-train.ann_constrs=\'qcfsconstrs\' \
+        snn-train.regularizer=\'none\' \
+        snn-train.multistep=False \
+        snn-train.add_time_dim=True \
+        snn-train.L=8 \
+        snn-train.T=10 \
+        snn-train.alpha=0.00 \
+        \
+        snn-test.sigma=1.0 \
+        snn-test.reset_mode='soft' \
+        snn-test.model_path=\'/LOCAL2/dengyu/MySNN/easycutoff/outputs/reg-before-act-cifar10-vgg16-ann1T4L-qcfsconstrs-roe-alpha0.002-seed1200-epochs300/cifar10-aideoserver.pth\'
+        
---
+++ examples/ANN_to_SNN/qcfs/cifar10/vgg16/training_alpha_0.002_8L.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python  ./snncutoff/scripts/training.py \
+        base.epochs=300 \
+        base.gpu_id=\'0\' \
+        base.seed=1200 \
+        base.port=\'13121\' \
+        base.data=\'cifar10\' \
+        base.model=\'vgg16\' \
+        base.dataset_path='datasets' \
+        \
+        snn-train.method=\'ann\' \
+        snn-train.ann_constrs=\'qcfsconstrs\' \
+        snn-train.regularizer=\'roe\' \
+        snn-train.multistep=True \
+        snn-train.add_time_dim=True \
+        snn-train.L=8 \
+        snn-train.T=1 \
+        snn-train.alpha=0.005
