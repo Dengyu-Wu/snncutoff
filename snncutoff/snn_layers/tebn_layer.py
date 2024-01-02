ca5c366ac4bed8a0dd01a904686f43c77ac5dd34
dengyu <d.wooo92@gmail.com>
2024-01-03 00:01:16 +0800

    update

--- snncutoff/snn_layers/tebn_layer.py
+++ snncutoff/snn_layers/tebn_layer.py
@@ -5,11 +5,11 @@ from .base_layer import BaseLayer
 class TEBN(BaseLayer):
     def __init__(self, T, num_features, eps=1e-5, momentum=0.1):
         super(TEBN, self).__init__()
-        self.bn = nn.BatchNorm3d(num_features)
+        self.bn = nn.BatchNorm2d(num_features)
         self.p = nn.Parameter(torch.ones(T, 1, 1, 1, 1))
         self.T = T
         
-    def forward(self, input):
+    def forward_mult_step(self, input):
         y = self.reshape(input)
         y = y.transpose(0, 1).contiguous()  # T N C H W ,  N T C H W
         y = y.transpose(1, 2).contiguous()  # N T C H W ,  N C T H W
@@ -20,3 +20,9 @@ class TEBN(BaseLayer):
         new_dim = [int(y.shape[1]*self.T),]
         new_dim.extend(y.shape[2:])
         return y.reshape(new_dim)
+    
+    def forward(self,x):
+        if self.multistep:
+            return self.forward_mult_step(x)
+        else:
+            TypeError('Single Step Inference is not supported for TEBN.')
