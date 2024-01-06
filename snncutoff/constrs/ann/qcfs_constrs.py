0edcb3cd15f7aaf01fb8b74895e2b4f62621215e
Dengyu Wu <d.wooo92@gmail.com>
2024-01-07 05:08:21 +0800

    refactor: delete redundant code

--- snncutoff/constrs/ann/qcfs_constrs.py
+++ snncutoff/constrs/ann/qcfs_constrs.py
@@ -1,12 +1,17 @@
 import torch
 from torch import nn
+from torch.autograd import Function
 from .base_constrs import BaseConstrs
 from typing import Callable, List, Type
 from snncutoff.gradients import GradFloor
 
-
 class QCFSConstrs(BaseConstrs):
-    def __init__(self, T: int = 4, L: int = 4, vthr: float = 8.0, tau: float = 1.0, gradient=GradFloor, regularizer: Type[nn.Module] = None, momentum=0.9):
+    def __init__(self, T: int = 4, 
+                 L: int = 4, 
+                 vthr: float = 8.0, 
+                 tau: float = 1.0, 
+                 gradient: Type[Function] = GradFloor, 
+                 regularizer: Type[nn.Module] = None, momentum=0.9):
         super().__init__()
         self.vthr = nn.Parameter(torch.tensor([vthr]), requires_grad=True)
         self.regularizer = regularizer
@@ -15,8 +20,7 @@ class QCFSConstrs(BaseConstrs):
         self.tau = tau
         self.momentum = momentum
         self.gradient = gradient.apply
-        self.relu = nn.ReLU()
-        
+        self.relu = nn.ReLU(inplace=True)
         
     def constraints(self, x):
         x = self.relu(x)
