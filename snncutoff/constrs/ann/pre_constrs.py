e9194f2cb289e2745795159970b7911b611dd6ca
Dengyu Wu <d.wooo92@gmail.com>
2024-01-07 05:08:21 +0800

    refactor: delete redundant code

--- snncutoff/constrs/ann/pre_constrs.py
+++ snncutoff/constrs/ann/pre_constrs.py
@@ -4,7 +4,10 @@ from typing import Callable, List, Type
 from .base_constrs import BaseConstrs
 
 class PreConstrs(BaseConstrs):
-    def __init__(self, T: int = 4, vthr: float = 8.0, module: Type[nn.Module] = None, momentum=0.9, multistep: bool = True):
+    def __init__(self, T: int = 4, 
+                 vthr: float = 8.0, 
+                 module: Type[nn.Module] = None, 
+                 multistep: bool = True):
         super().__init__()
         self.T = T
         self.module = module
