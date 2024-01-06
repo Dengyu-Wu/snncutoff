a44eb7ea74aea883abc0ab5ca3b1d952ab5eba4e
dengyu <d.wooo92@gmail.com>
2024-01-07 05:00:00 +0800

    refactor: rename regularizer for readability

--- snncutoff/constrs/ann/base_constrs.py
+++ snncutoff/constrs/ann/base_constrs.py
@@ -1,11 +1,9 @@
 import torch
 from torch import nn
-from torch.autograd import Function
-from snncutoff.regularizer import ROE
 from typing import Callable, List, Type
 
 class BaseConstrs(nn.Module):
-    def __init__(self, T: int = 4, L: int = 4, vthr: float = 8.0, tau: float = 1.0, regularizer: Type[ROE] = None, momentum=0.9):
+    def __init__(self, T: int = 4, L: int = 4, vthr: float = 8.0, tau: float = 1.0, regularizer: Type[nn.Module] = None, momentum=0.9):
         super().__init__()
         self.vthr = nn.Parameter(torch.tensor([vthr]), requires_grad=False)
         self.regularizer = regularizer
