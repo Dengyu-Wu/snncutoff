171fe3960735723c62f38f713eeda3efe8575c86
Dengyu Wu <d.wooo92@gmail.com>
2024-01-23 23:09:40 +0800

    feat: remove tiny imagenet, instead we are supoort subset of imagenet by using imagenet-N, i.e., N is the number of class.

--- snncutoff/datasets/__init__.py
+++ snncutoff/datasets/__init__.py
@@ -1,5 +1,4 @@
 from .get_cifar10 import GetCifar10
 from .get_cifar100 import GetCifar100
 from .get_imagenet import GetImageNet
-from .get_tiny_imagenet import GetTinyImageNet
 from .get_dvs import GetDVS
