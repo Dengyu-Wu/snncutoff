4ab67eadbd507c6e0b03b3e05e160cd9047ef8ac
Dengyu Wu <d.wooo92@gmail.com>
2024-01-25 00:15:07 +0800

    fix: updated forvaluation

--- snncutoff/scripts/evaluation.py
+++ snncutoff/scripts/evaluation.py
@@ -61,16 +61,13 @@ def main(cfg: DictConfig):
     timesteps=[]
     samples_number=[]
     for i in range(1):
-        epsilon = args.epsilon
-        # epsilon = 0.05*i
+        epsilon = 0.05*i
         _acc, _timesteps, _samples_number = evaluator.cutoff_evaluation(test_loader,train_loader=train_loader,epsilon=epsilon)
         acc.append(_acc)
         timesteps.append(_timesteps)
         samples_number.append(_samples_number)
     acc = np.array(acc)
     timesteps = np.array(timesteps)
-    print(acc)
-    print(timesteps)
     result={'accuracy': acc, 'timesteps': timesteps, 'samples_number':samples_number}
     save_pickle(result,name='topk_cutoff',path=os.path.dirname(path))
 
