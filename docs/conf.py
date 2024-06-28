144563ca2c0d2850772fd6d88aac4abafbee66be
Dengyu Wu <d.wooo92@gmail.com>
2024-06-29 01:32:00 +0800

    doc: doc update

--- docs/conf.py
+++ docs/conf.py
@@ -26,8 +26,6 @@ myst_enable_extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-mathjax_path="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
