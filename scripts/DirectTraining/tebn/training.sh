4b22d847c144ba5ec90a8e07669d01772e2d47c0
Dengyu Wu <d.wooo92@gmail.com>
2024-07-18 05:38:35 +0800

    add: add neuron_params to examples

--- scripts/DirectTraining/tebn/training.sh
+++ scripts/DirectTraining/tebn/training.sh
@@ -11,12 +11,19 @@ python  ./scripts/training.py \
         base.dataset_path=\'datasets\' \
         \
         snn-train.method=\'snn\' \
-        snn-train.ann_constrs=\'baseconstrs\' \
+        snn-train.ann_constrs=\'none\' \
+        snn-train.arch_conversion=True \
         snn-train.snn_layers=\'baselayer\' \
         snn-train.regularizer=\'rcs\' \
-        snn-train.TET=False \
+        snn-train.loss='tet' \
         snn-train.TEBN=True \
         snn-train.multistep=True \
         snn-train.add_time_dim=True \
-        snn-train.T=6 \
-        snn-train.alpha=0.00
+        snn-train.T=2 \
+        snn-train.alpha=0.00 \
+        \
+        neuron_params.vthr=1.0 \
+        neuron_params.tau=0.5 \
+        neuron_params.mem_init=0.0 \
+        neuron_params.multistep=True \
+        neuron_params.reset_mode='hard' 
--- scripts/DirectTraining/tet/training.sh
+++ scripts/DirectTraining/tet/training.sh
@@ -3,20 +3,28 @@
 python  ./scripts/training.py \
         base.batch_size=128 \
         base.epochs=300 \
-        base.gpu_id=\'1\' \
+        base.gpu_id=\'0\' \
         base.seed=1200 \
         base.port=\'11152\' \
         base.data=\'cifar10\' \
         base.model=\'sew_resnet18\' \
         base.dataset_path=\'datasets\' \
+        base.checkpoint_save=False \
+        base.checkpoint_path=none \
         \
         snn-train.method=\'snn\' \
+        snn-train.ann_constrs=\'none\' \
         snn-train.arch_conversion=False \
-        snn-train.ann_constrs=\'baseconstrs\' \
-        snn-train.snn_layers=\'simplebaselayer\' \
+        snn-train.snn_layers=\'baselayer\' \
         snn-train.regularizer=\'rcs\' \
-        snn-train.TET=True \
+        snn-train.loss='tet' \
         snn-train.multistep=True \
         snn-train.add_time_dim=True \
-        snn-train.T=6 \
-        snn-train.alpha=0.00
+        snn-train.T=2 \
+        snn-train.alpha=0.00 \
+        \
+        neuron_params.vthr=1.0 \
+        neuron_params.tau=0.5 \
+        neuron_params.mem_init=0.0 \
+        neuron_params.multistep=True \
+        neuron_params.reset_mode='hard' 
